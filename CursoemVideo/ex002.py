74223f067ef0107b3d519c70d1659bfea94b8bca
brdavidbraga <brdavidbraga@gmail.com>
2019-03-23 05:33:40 -0300

    importacao inicial

---
+++ CursoemVideo/ex002.py
@@ -0,0 +1,5 @@
+n1 = int(input('Digite o primeiro numero: '))
+n2 = int(input('Digite o segundo numero numero: '))
+s = n1 + n2
+print('Soma entre ',n1, 'e',n2, 'vale : ', s)
+print('A Soma entre {} e {} vale :{}'.format(n1, n2, s) )
